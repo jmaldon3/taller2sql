3a607c90327877b3b7c92bded2cc817ce7e706cd
jmaldon3 <joanmaldovillada@gmail.com>
2025-05-29 17:27:15 -0500

    Add files via upload

--- taller 2 SQL/Punto 4.sql
+++ taller 2 SQL/Punto 4.sql
@@ -1,10 +1,11 @@
 SELECT v.CreatedAt AS fecha,
 p.id AS producto_id, 
+p.nombre,
 p.cantidad AS cantidad_inicial,
 (p.cantidad - SUM(dv.cantidad)) AS stock_restante
 FROM producto p
 JOIN detalle_venta dv ON p.id = dv.producto_id
 JOIN venta v ON dv.id = v.detalle_venta_id  
-WHERE p.id = 1  -- aca ponemos el id que querramos consultar 
+WHERE p.id = 2  -- aca ponemos el id que querramos consultar 
 GROUP BY v.CreatedAt, p.id
 ORDER BY v.CreatedAt;
